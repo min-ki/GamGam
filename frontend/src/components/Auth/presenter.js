d72f37146838f42ef9a3924f2640630c4e2825a1
zhsks528 <hsm950825@naver.com>
2018-10-27 18:12:23 +0900

    Auth 스타일 수정

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -6,36 +6,36 @@ import SignupForm from "components/SignupForm";
 import Card from "components/Card";
 
 const Auth = (props, context) => (
-  <main className={styles.auth}>
-    <div className={styles.column2}>
-      <div className={`${styles.whiteBox} ${styles.formBox}`}>
-        <img src={require("images/logo.png")} alt="Logo" />
-        {props.action === "login" && <LoginForm />}
-        {props.action === "signup" && <SignupForm />}
-      </div>  
-      <div className={styles.whiteBox}>
-        {props.action === "login" && (
-          <p>
-            {context.t("Don't have an account?")}{" "}
-            <span className={styles.changeLink} onClick={props.changeAction}>
-                    {context.t("Sign up")}
-            </span>
-          </p>
-        )}
-        {props.action === "signup" && (
-          <p>
-            {context.t("Have an account?")}{" "}
-            <span className={styles.changeLink} onClick={props.changeAction}>
-                {context.t("Log in")}
-            </span>
-          </p>
-        )}
+    <main className={styles.auth}>
+      <div className={styles.column2}>
+        <div className={`${styles.whiteBox} ${styles.formBox}`}>
+          <img src={require("images/logo.png")} alt="Logo" />
+          {props.action === "login" && <LoginForm />}
+          {props.action === "signup" && <SignupForm />}
+        </div>  
+        <div className={styles.whiteBox}>
+          {props.action === "login" && (
+            <p>
+              {context.t("Don't have an account?")}{" "}
+              <span className={styles.changeLink} onClick={props.changeAction}>
+                      {context.t("Sign up")}
+              </span>
+            </p>
+          )}
+          {props.action === "signup" && (
+            <p>
+              {context.t("Have an account?")}{" "}
+              <span className={styles.changeLink} onClick={props.changeAction}>
+                  {context.t("Log in")}
+              </span>
+            </p>
+          )}
+        </div>
       </div>
-    </div>
-    <div className={styles.column1}>
-      <Card />
-    </div>
-  </main>
+      <div className={styles.column1}>
+        <Card />
+      </div>
+    </main>
 );
 
 Auth.contextTypes = {
