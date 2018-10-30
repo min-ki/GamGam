44218311d63b6ba2eaec367792a577724f4e0cb3
bluebug11 <zlwbgo@naver.com>
2018-10-30 21:51:51 +0900

    타임라인 레이아웃 수정

--- frontend/src/components/TimeLine/presenter.js
+++ frontend/src/components/TimeLine/presenter.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { Timeline, Event } from "react-timeline-scribble";
 import styles from "./styles.scss";
 import Loading from "components/Loading";
-import { ButtonToolbar } from "reactstrap";
+import { ButtonToolbar, Media } from "reactstrap";
+
 
 const TimeLine = props => {
   if (props.loading) {
@@ -14,6 +15,7 @@ const TimeLine = props => {
 
 const MapToTimeline = props => (
   <div className="timeline">
+    <div className = "timline-description">추억하기</div>
     {props.feed.filter(travel => travel.status === "추억하기").map(travel => (
       <RenderTimeLine {...travel} key={travel.id} />
     ))}
@@ -21,12 +23,13 @@ const MapToTimeline = props => (
 );
 
 const RenderTimeLine = props => {
+  
   return (
     <div className="timeline-wrapper">
       <div className="timeline-content">
         <Timeline>
           <Event
-            interval={"2016 – 2018"}
+            interval={`${props.start_at} ~ ${props.end_at}`}
             title={`${props.title}`}
             subtitle={`${props.tags}`}
           >
@@ -39,9 +42,22 @@ const RenderTimeLine = props => {
             />
             <br />
             여행경비 : {props.price}
-            <ButtonToolbar className={styles.button}>수정</ButtonToolbar>
           </Event>
         </Timeline>
+      </div>
+      <div className = "timeline-image" >
+        <div className = "timeline-image-center">
+          {props.travel_plan.map(plan => (
+            <img className ="timeline-image-move" src={plan.plan_images[0] ? plan.plan_images[0].file : require("images/logo.png")} alt="temp" width="100" height="100"/>
+          ))}
+            {/*
+            <div className ="TimeLineList">
+            <h1 className="TimeLineList-content">제목 : {plan.title}</h1>
+            <h1 className="TimeLineList-content">일자 : {plan.travel_day}</h1>
+            <h1 className="TimeLineList-content">내용 : {plan.content}</h1>
+            </div>
+            */}
+        </div>
       </div>
     </div>
   );
