e16c800d9e310df36a29324e2543df8d3b480fa2
min_2271 <min_2271@naver.com>
2019-06-02 23:28:47 +0900

    Travel Model 교통수단 필드 추가

--- GamGam/travel/models.py
+++ GamGam/travel/models.py
@@ -56,13 +56,13 @@ class Travel(TimeStampedModel):
         options={'quality': 60}
     )
 
-    title = models.CharField(max_length=50) # 여행 계획 제목
+    title = models.CharField(max_length=50, blank=True, null=True) # 여행 계획 제목
     status = models.CharField(max_length=1, choices=TRAVEL_STATUS_CHOICE, default="0") # 여행 상태
     owner = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name='travel')
     price = models.IntegerField(default=0, blank=True) # 여행 비용
    
 
-    travel_region = models.CharField(max_length=30) # 여행 국가
+    travel_region = models.CharField(max_length=30, blank=True, null=True) # 여행 국가
     korea_travel_region = models.CharField(max_length=2, choices=TRAVEL_REGION, default="0")
     start_at = models.DateField() # 여행 시작일
     end_at = models.DateField()  # 여행 종료일
@@ -83,13 +83,23 @@ class Travel(TimeStampedModel):
 # todo: 여행 계획 리스트를 만들기위해서는 각 리스트를 담아야할 클래스 있어야함 -> TravelPlan
 class TravelPlan(models.Model):
 
-    title = models.CharField(max_length=50) # 세부 여행 계획 제목
-    content = models.TextField()
-    travel = models.ForeignKey(Travel, on_delete=models.CASCADE, related_name='travel_plan', null=True)
-    travel_region = models.CharField(max_length=30)  # 각 계획의 여행 지역
-    travel_day = models.DateField() # 여행 일
-    price = models.IntegerField(default=0) # 각각 계획 마다의 비용
+    TRAVEL_TRANSPORTATION_CHOICE = (
+        ("0", '도보'),
+        ("1", '자전거'),
+        ("2", '버스'),
+        ("3", '지하철'),
+        ("4", '비행기'),
+        ("5", '자차'),
+        ("6", '배'),
+    )
 
+    title = models.CharField(max_length=50, blank=True, null=True) # 세부 여행 계획 제목
+    content = models.TextField(blank=True, null=True)
+    travel = models.ForeignKey(Travel, on_delete=models.CASCADE, related_name='travel_plan', null=True)
+    travel_region = models.CharField(max_length=30, blank=True, null=True)  # 각 계획의 여행 지역
+    travel_day = models.DateField() # 여행 일
+    price = models.IntegerField(default=0, blank=True, null=True) # 각각 계획 마다의 비용
+    Transportation = models.CharField(max_length=2, choices=TRAVEL_TRANSPORTATION_CHOICE, default="0")
 
     class Meta:
         ordering = ['travel_day']
@@ -120,8 +130,8 @@ class Image(models.Model):
 
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name='images')
-    plan = models.ForeignKey(TravelPlan, on_delete=models.PROTECT, null=True, related_name='plan_images')
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, blank=True, null=True, related_name='images')
+    plan = models.ForeignKey(TravelPlan, on_delete=models.PROTECT, blank=True, null=True, related_name='plan_images')
     tags = TaggableManager()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
